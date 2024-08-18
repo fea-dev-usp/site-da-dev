87c7e0e377560b44c91f2b3a4d74b6bab64b4a42
Andre <andrepennini@gmail.com>
2024-08-18 14:07:24 -0300

    Todas as páginas com django template sintaxe e com as urls preparadas

--- sitedev/feadev/urls.py
+++ sitedev/feadev/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sobre/', views.sobre, name='sobre'),
+    path('contato/', views.contato, name='contato'),
+    path('projetos/', views.projetos, name='projetos'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('grupos/', views.grupos, name='grupos'),
+    path('cursos/', views.cursos, name='cursos'),
+    path('processo_seletivo/', views.processo_seletivo, name='processo_seletivo'),
 ]
--- sitedev/feadev/views.py
+++ sitedev/feadev/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'feadev/index.html')
+
+def sobre(request):
+    return render(request, 'feadev/sobre.html')
+
+def contato(request):
+    return render(request, 'feadev/contato.html')
+
+def projetos(request):
+    return render(request, 'feadev/projetos.html')
+
+def eventos(request):
+    return render(request, 'feadev/eventos.html')
+
+def grupos(request):
+    return render(request, 'feadev/grupos.html')
+
+def cursos(request):
+    return render(request, 'feadev/cursos.html')
+
+def processo_seletivo(request):
+    return render(request, 'feadev/processo_seletivo.html')
